71cb0d3e91000885f077f7e08affeb2bf41b72a1
ankit <ankitlovanshi622@gmail.com>
2021-10-13 11:26:05 +0530

    Add home button on show page

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -10,7 +10,7 @@ class CommentsController < ApplicationController
     @comment = @article.comments.find(params[:id])
     @comment.destroy
     redirect_to article_path(@article)
-  end
+  end 
 
   private
   
